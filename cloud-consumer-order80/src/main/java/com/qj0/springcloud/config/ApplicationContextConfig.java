78b473b1e504fa365dda39f4d29b86941624ddf6
qj0 <qj0kyo1122@gmail.com>
2020-09-30 15:33:45 +0800

    完成zookeeper,consul模块的基本编写

--- cloud-consumer-order80/src/main/java/com/qj0/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/qj0/springcloud/config/ApplicationContextConfig.java
@@ -1,12 +1,13 @@
 package com.qj0.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationContextConfig {
-
+    @LoadBalanced
     @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
---
+++ cloud-provider-payment8002/src/main/java/com/qj0/springcloud/dao/PaymentDao.java
@@ -0,0 +1,13 @@
+package com.qj0.springcloud.dao;
+
+import com.qj0.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface PaymentDao {
+
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+}
